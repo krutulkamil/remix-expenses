ded713549504043e982b67ae28e40d2768cbbb06
krutulkamil <krutulkamil@onet.pl>
2023-01-14 19:59:49 +0100

    fix: add global custom Google Font

--- app/root.tsx
+++ app/root.tsx
@@ -24,6 +24,10 @@ const App = () => {
         <html lang="en">
         <head>
             <Meta />
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+            <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;600;700&display=swap"
+                  rel="stylesheet" />
             <Links />
         </head>
         <body>
