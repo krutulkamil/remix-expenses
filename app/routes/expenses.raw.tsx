d2a298b02347254b1c3d1a6945059a167f22e7da
krutulkamil <krutulkamil@onet.pl>
2023-01-15 19:30:29 +0100

    feat: return raw expenses resource route

--- app/routes/expenses.raw.tsx
+++ app/routes/expenses.raw.tsx
@@ -1,16 +1,4 @@
-import type { MetaFunction } from "@remix-run/node";
-import type { FunctionComponent } from "react";
+import type { LoaderFunction } from "@remix-run/node";
+import { DUMMY_EXPENSES } from "~/routes/__app/expenses.analysis";
 
-const RawExpensesPage: FunctionComponent = (): JSX.Element => {
-    return (
-        <h1>Raw Expenses Page</h1>
-    );
-};
-
-export const meta: MetaFunction = () => ({
-    charset: "utf-8",
-    title: "Raw Expenses | Remix Expenses",
-    viewport: "width=device-width,initial-scale=1"
-});
-
-export default RawExpensesPage;
+export const loader: LoaderFunction = () => DUMMY_EXPENSES;
