61059537bd45dd3f55b1efa3f282278cfbeece0b
krutulkamil <krutulkamil@onet.pl>
2023-01-17 17:37:02 +0100

    feat: handling analysis page without expenses

--- app/routes/__app/expenses.analysis.tsx
+++ app/routes/__app/expenses.analysis.tsx
@@ -1,11 +1,13 @@
+import { useCatch, useLoaderData } from "@remix-run/react";
+import { json } from "@remix-run/node";
 import ExpenseStatistics from "~/components/expenses/ExpenseStatistics";
 import Chart from "~/components/expenses/Chart";
+import Error from "~/components/util/Error";
 import { getExpenses } from "~/data/expenses.server";
-import { useLoaderData } from "@remix-run/react";
-import type { MetaFunction } from "@remix-run/node";
 import type { FunctionComponent } from "react";
 import type { Expense as IExpense } from "@prisma/client";
 import type { LoaderFunction } from "@remix-run/node";
+import type { CatchBoundaryComponent } from "@remix-run/react/dist/routeModules";
 
 const ExpensesAnalysisPage: FunctionComponent = (): JSX.Element => {
     const expenses: IExpense[] = useLoaderData<typeof loader>();
@@ -19,13 +21,28 @@ const ExpensesAnalysisPage: FunctionComponent = (): JSX.Element => {
 };
 
 export const loader: LoaderFunction = async () => {
-    return await getExpenses();
+    const expenses: IExpense[] = await getExpenses();
+
+    if (!expenses || expenses.length === 0) {
+        throw json({ message: "Could not load expenses for the requested analysis." }, {
+            status: 404,
+            statusText: "Expenses not found."
+        });
+    }
+
+    return expenses;
 };
 
-export const meta: MetaFunction = () => ({
-    charset: "utf-8",
-    title: "Analysis | Remix Expenses",
-    viewport: "width=device-width,initial-scale=1"
-});
+export const CatchBoundary: CatchBoundaryComponent = (): JSX.Element => {
+    const caughtResponse = useCatch();
+
+    return (
+        <main>
+            <Error title={caughtResponse.statusText}>
+                <p>{caughtResponse.data?.message || "Something went wrong - could not load expenses."}</p>
+            </Error>
+        </main>
+    );
+};
 
 export default ExpensesAnalysisPage;
